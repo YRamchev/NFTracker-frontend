8206f201757bf7ea82230fd3625accc30d174fc3
yordan.ramchev <yordan.ramchev@mentormate.com>
2022-05-07 16:47:10 +0300

    project init

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const router = createRouter({
       name: "home",
       component: () => import("../views/HomeView.vue"),
     },
+    {
+      path: "/auth/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
+    },
   ],
 });
 
